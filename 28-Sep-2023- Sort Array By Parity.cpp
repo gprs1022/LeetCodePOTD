a8ef48190c8d7f16827e595f361d2c38ff92a92b
gprs1022 <pradeep1022gprs@gmail.com>
2023-09-28 08:49:50 +0530

    Sort Array By Parity

---
+++ 28-Sep-2023- Sort Array By Parity.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    std::vector<int> sortArrayByParity(std::vector<int>& nums) {
+        std::vector<int> result;
+        for (int i : nums) {
+            if (i % 2 == 0) {
+                result.push_back(i);
+            }
+        }
+        for (int j : nums) {
+            if (j % 2 != 0) {
+                result.push_back(j);
+            }
+        }
+        return result;
+    }
+};
