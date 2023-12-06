765027853b2ca0107095fb9f48ca6dd44903845f
gprs1022 <pradeep1022gprs@gmail.com>
2023-12-06 05:48:31 +0530

    .

--- 3-Nov-2023- Pythagorean Triplet.cpp
+++ 3-Nov-2023- Pythagorean Triplet.cpp
@@ -1,52 +1,22 @@
-//Pythagorean Triplet
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-// } Driver Code Ends
-//User function template for C++
 class Solution{
 public:
 	// Function to check if the
 	// Pythagorean triplet exists or not
 	bool checkTriplet(int arr[], int n) {
 	    // code here
-	      unordered_map<int,bool> mp;
-        for(int i=0;i<n;i++){
-            mp[arr[i]*arr[i]]=1;
-        }
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(mp[arr[i]*arr[i] + arr[j]*arr[j]]==1 ) return true;
-            }
-        }
-        return false;
-        
+	    unordered_map<long long, bool> mp;
+	    for(int i=0;i<n;i++){
+	        mp[arr[i]*arr[i]]= true;
+	    }
+	    
+	    for(int i=0;i<n;i++){
+	        for(int j=i+1;j<n;j++){
+	           long long sum= arr[i]* arr[i] +arr[j]*arr[j];
+	           if(mp[sum]){
+	               return true;
+	           }
+	        }
+	    }
+	    return false;
 	}
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, i;
-        cin >> n;
-        int arr[n];
-        for (i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        Solution ob;
-        auto ans = ob.checkTriplet(arr, n);
-        if (ans) {
-            cout << "Yes\n";
-        } else {
-            cout << "No\n";
-        }
-    }
-    return 0;
-}
-
-// } Driver Code Ends
