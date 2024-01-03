1b5c43862b95ee1c233f551232269b86a599b231
gprs1022 <pradeep1022gprs@gmail.com>
2024-01-03 12:49:28 +0530

    Smallest window containing 0, 1, and 2

---
+++ Dec 2023/Jan 2024/3-Jan-2024-Smallest window containing 0,1 and 2.cpp
@@ -0,0 +1,60 @@
+class Solution {
+  public:
+    int smallestSubstring(string &s) {
+        // Code here
+        s += '3';
+        int n = s.length();
+        int num0 = 0;
+        int num1 = 0;
+        int num2 = 0;
+        
+        int i = 0;
+        int j = 0;
+        
+        int maxi = INT_MAX;
+        
+        while(j < n + 1)
+        {
+            if(num0 > 0 && num1 > 0 && num2 > 0)
+            {
+                int curr = (j - 1 - i + 1);
+                if(curr >= 3) maxi = min(maxi, curr);
+                if(s[i] == '0')
+                {
+                    num0--;
+                }
+                else if(s[i] == '1')
+                {
+                    num1--;
+                }
+                else if(s[i] == '2')
+                {
+                    num2--;
+                }
+                i++;
+                //maxi--;
+            }
+            
+            
+            else
+            {
+                if(s[j] == '0')
+                {
+                    num0++;
+                }
+                else if(s[j] == '1')
+                {
+                    num1++;
+                }
+                else if(s[j] == '2')
+                {
+                    num2++;
+                }
+                j++;
+           // maxi++;
+            }
+        }
+        return (maxi == INT_MAX)? -1 : maxi;
+    }
+};
+
